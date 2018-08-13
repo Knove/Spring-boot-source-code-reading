aa23d99c91a1c869940176f5643dc0657e0abdd1
KnoveZ <308346293@qq.com>
2018-08-13 20:33:03 +0800

    部署基本项目

---
+++ src/main/java/com/knove/knove/KnoveApplication.java
@@ -0,0 +1,26 @@
+package com.knove.knove;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class KnoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnoveApplication.class, args);
+	}
+}
+@RestController
+class MessageRestController {
+
+	@Value("${message:Hello default}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		return this.message;
+	}
+}
